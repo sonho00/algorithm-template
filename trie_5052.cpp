60307c86d240572aeff89c31e7b79d9fda5384a5
sonho00 <sonho00@naver.com>
2020-05-09 22:53:34 +0900

    substracting default value inside trie

--- trie_5052.cpp
+++ trie_5052.cpp
@@ -20,27 +20,27 @@ struct node{
     node(ll n):cnt(0),end(0),idx(vector<ll>(n)){} 
 };
 struct TRIE{
-    ll n;
+    ll n,def;
     vector<node> v;
-    TRIE(ll n):n(n),v(vector<node>(1,n)){}
+    TRIE(ll n,ll def):n(n),def(def),v(vector<node>(1,n)){}
     ll find(string& s){
         ll sz=s.size()-1;
         ll vi=0;
         FOR(i,0,sz){
-            ll nxt=v[vi].idx[s[i]];
+            ll nxt=v[vi].idx[s[i]-def];
             if(nxt) vi=nxt;
             else return 0;
         }
-        return v[vi].cnt;
+        return v[vi].cnt!=1;
     }
     void add(string& s){
         ll sz=s.size()-1;
         ll vi=0;
         ++v[0].cnt;
         FOR(i,0,sz){
-            ll nxt=v[vi].idx[s[i]];
+            ll nxt=v[vi].idx[s[i]-def];
             if(!nxt){
-                nxt=v[vi].idx[s[i]]=v.size();
+                nxt=v[vi].idx[s[i]-def]=v.size();
                 v.eb(n);
             }
             vi=nxt;
@@ -51,13 +51,12 @@ struct TRIE{
 };
 
 void tc(){
-    TRIE trie(10);
+    TRIE trie(10,'0');
     ll n;
     cin>>n;
     string str[n+5];
     FOR(i,1,n){
         cin>>str[i];
-        for(char& ch:str[i]) ch-='0';
         trie.add(str[i]);
     }
     FOR(i,1,n){
