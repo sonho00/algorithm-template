836939081d18c8d478183ac3d08fc6da6806d163
sonho00 <sonho00@naver.com>
2020-05-18 17:40:12 +0900

    edit template

--- dinic_with_mincut_13161.cpp
+++ dinic_with_mincut_13161.cpp
@@ -1,32 +1,37 @@
+#define _USE_MATH_DEFINES
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(ll i=a; i<=b; ++i)
 #define ALL(x) x.begin(),x.end()
+#define gcd __gcd
 #define pb push_back
 #define eb emplace_back
 #define fi first
 #define se second
+#define endl '\n'
 using namespace std;
 using ll=long long;
 using pll=pair<ll,ll>;
+using vl=vector<ll>;
+using vp=vector<pll>;
 const ll INF=1e18;
-const ll MAX=500;
+const ll MAX=1e5;
 const ll MOD=1e9+7;
-const ll LOG=20;
 
 struct maxfl{
     ll n;
-    vector<ll> side,work,lv;
-    vector<vector<ll>> cap,flow;
+    vl side,work,lv;
+    vector<vl> cap,flow;
     maxfl(ll n):n(n),
-    cap(vector<vector<ll>>(n+5,vector<ll>(n+5))),
-    flow(vector<vector<ll>>(n+5,vector<ll>(n+5))){}
+    cap(vector<vl>(n+5,vl(n+5))),
+    flow(vector<vl>(n+5,vl(n+5))){}
     bool bfs(){
-        lv=vector<ll>(n+5);
+        lv=vl(n+5);
         queue<ll> q;
         q.push(n+1);
         lv[n+1]=1;
         while(!q.empty()){
             ll top=q.front();
+            q.pop();
             FOR(i,1,n+2){
                 if(!lv[i] && flow[top][i]<cap[top][i]){
                     lv[i]=lv[top]+1;
@@ -42,10 +47,13 @@ struct maxfl{
         ll ret=0,rest;
         for(ll& i=work[idx]; i<=n+2; ++i){
             rest=cap[idx][i]-flow[idx][i];
-            if(lv[i]==lv[idx]+1 && rest && (ret=dfs(i,min(f,rest)))){
-                flow[idx][i]+=ret;
-                flow[i][idx]-=ret;
-                return ret;
+            if(lv[i]==lv[idx]+1 && rest){
+                ret=dfs(i,min(f,rest));
+                if(ret){
+                    flow[idx][i]+=ret;
+                    flow[i][idx]-=ret;
+                    return ret;
+                }
             }
         }
         return 0;
@@ -53,23 +61,23 @@ struct maxfl{
     ll operator()(){
         ll ret=0,d;
         while(bfs()){
-            work=vector<ll>(n+5,1);
+            work=vl(n+5,1);
             while(d=dfs()) ret+=d;
         }
         return ret;
     }
     void cut(){
-        side=vector<ll>(n+5);
+        side=vl(n+5);
         side[n+1]=1;
-        ll q[MAX+5];
-        ll s=0,e=0;
-        q[e++]=n+1;
-        while(s<e){
-            ll top=q[s++];
+        queue<ll> q;
+        q.push(n+1);
+        while(!q.empty()){
+            ll top=q.front();
+            q.pop();
             FOR(i,1,n+2){
                 if(!side[i] && flow[top][i]<cap[top][i]){
                     side[i]=1;
-                    q[e++]=i;
+                    q.push(i);
                 }
             }
         }
@@ -95,11 +103,11 @@ int main(){
         }
     }
     FOR(i,1,n) FOR(j,1,n) cin>>dinic.cap[i][j];
-    cout<<dinic()<<'\n';
+    cout<<dinic()<<endl;
     dinic.cut();
     FOR(i,1,n) if(dinic.side[i]==1) cout<<i<<' ';
-    cout<<'\n';
+    cout<<endl;
     FOR(i,1,n) if(dinic.side[i]!=1) cout<<i<<' ';
-    cout<<'\n';
+    cout<<endl;
     return 0;
 }
