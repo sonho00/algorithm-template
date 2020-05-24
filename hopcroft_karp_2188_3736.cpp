3a7d91e55fa3394745672866e45e206558863b6b
sonho00 <sonho00@naver.com>
2020-05-24 20:42:38 +0900

    vector<ll> to vl

--- hopcroft_karp_2188_3736.cpp
+++ hopcroft_karp_2188_3736.cpp
@@ -1,29 +1,31 @@
+#define _USE_MATH_DEFINES
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(ll i=a; i<=b; ++i)
 #define ALL(x) x.begin(),x.end()
+#define gcd __gcd
 #define pb push_back
 #define eb emplace_back
 #define fi first
 #define se second
+#define endl '\n'
 using namespace std;
 using ll=long long;
 using pll=pair<ll,ll>;
+using vl=vector<ll>;
+using vp=vector<pll>;
 const ll INF=1e18;
-const ll MAX=1e4;
+const ll MAX=1e5;
 const ll MOD=1e9+7;
-const ll LOG=20;
 
 struct bimat{
     ll n,m;
-    vector<vector<ll>> adj;
-    vector<ll> mata,matb,lv;
+    vl mata,matb,lv;
+    vector<vl> adj;
     bimat(ll n,ll m):n(n),m(m),
-    adj(vector<vector<ll>>(n+5)),
-    mata(vector<ll>(n+5)),
-    matb(vector<ll>(m+5))
-    {}
-    void hop_bfs(){
-        lv=vector<ll>(n+5);
+    mata(vl(n+5)),matb(vl(m+5)),
+    adj(vector<vl>(n+5)){}
+    void bfs(){
+        lv=vl(n+5);
         queue<ll> q;
         FOR(i,1,n){
             if(!mata[i]){
@@ -43,10 +45,10 @@ struct bimat{
             }
         }
     }
-    bool hop_dfs(ll idx){
+    bool dfs(ll idx){
         for(ll i:adj[idx]){
             ll nxt=matb[i];
-            if(!nxt || lv[nxt]==lv[idx]+1 && hop_dfs(nxt)){
+            if(!nxt || lv[nxt]==lv[idx]+1 && dfs(nxt)){
                 matb[i]=idx;
                 mata[idx]=i;
                 return true;
@@ -57,10 +59,10 @@ struct bimat{
     ll operator()(){
         ll ret=0;
         while(1){
-            hop_bfs();
-            ll tmp=0;
-            FOR(i,1,n) if(!mata[i] && hop_dfs(i)) ++tmp;
-            if(tmp) ret+=tmp;
+            bfs();
+            ll cnt=0;
+            FOR(i,1,n) if(!mata[i] && dfs(i)) ++cnt;
+            if(cnt) ret+=cnt;
             else break;
         }
         return ret;
