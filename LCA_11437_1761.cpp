203fe0f984408cc625f8f15e3670f9fdbe1be076
sonho00 <sonho00@naver.com>
2020-06-09 00:15:41 +0900

    delete graph struct

--- LCA_11437_1761.cpp
+++ LCA_11437_1761.cpp
@@ -12,85 +12,80 @@ using pll=pair<ll,ll>;
 using vl=vector<ll>;
 using vp=vector<pll>;
 const ll INF=1e18;
+const ll MAXV=4e4;
+const ll LOG=20;
 
-ll lb(ll x){
-    ll ret=-1;
-    while(x){
-        x>>=1;
-        ++ret;
+ll n;
+vp adj[MAXV+5];
+pll anc[MAXV+5][LOG+5];
+ll lv[MAXV+5];
+
+void bfs(ll idx){
+    lv[idx]=1;
+    queue<ll> q;
+    q.push(idx);
+    while(!q.empty()){
+        ll top=q.front();
+        q.pop();
+        for(pll& p:adj[top]){
+            ll a,b;
+            tie(a,b)=p;
+            if(lv[b]) continue;
+            lv[b]=lv[top]+1;
+            q.push(b);
+            anc[b][0]={a,top};
+        }
     }
+    FOR(j,1,LOG) FOR(i,1,n){
+        ll a,b;
+        tie(a,b)=anc[i][j-1];
+        ll c,d;
+        tie(c,d)=anc[b][j-1];
+        anc[i][j]={a+c,d};
+    }
+}
+void goai(pll& ret,ll& a,ll i){
+    pll& p=anc[a][i];
+    ret.fi+=p.fi;
+    ret.se=a=p.se;
+}
+pll lca(ll a,ll b){
+    pll ret(0,a);
+    if(lv[a]>lv[b]) swap(a,b);
+    for(ll diff=lv[b]-lv[a],i=0; diff; ++i){
+        if(diff&1<<i){
+            diff^=1<<i;
+            goai(ret,b,i);
+        }
+    }
+    if(a==b) return ret;
+    for(ll i=LOG; i>=0; --i){
+        if(anc[a][i].se!=anc[b][i].se){
+            goai(ret,a,i);
+            goai(ret,b,i);
+        }
+    }
+    goai(ret,a,0);
+    goai(ret,b,0);
     return ret;
 }
-struct graph{
-    ll n,log;
-    vector<vp> adj,anc;
-    vl lv;
-    graph(ll n):n(n),log(lb(n)),adj(vector<vp>(n+5)){}
-    void bfs(ll idx){
-        anc=vector<vp>(n+5,vp(log+5));
-        lv=vl(n+5);
-        lv[idx]=1;
-        queue<ll> q;
-        q.push(idx);
-        while(!q.empty()){
-            ll top=q.front();
-            q.pop();
-            for(pll& p:adj[top]){
-                if(lv[p.se]) continue;
-                lv[p.se]=lv[top]+1;
-                q.push(p.se);
-                anc[p.se][0]={p.fi,top};
-            }
-        }
-        FOR(j,1,log+3) FOR(i,1,n){
-            ll a,b;
-            tie(a,b)=anc[i][j-1];
-            anc[i][j]={a+anc[b][j-1].fi,anc[b][j-1].se};
-        }
-    }
-    void goai(pll& ret,ll& a,ll i){
-        pll& p=anc[a][i];
-        ret.fi+=p.fi;
-        ret.se=a=p.se;
-    }
-    pll lca(ll a,ll b){
-        pll ret(0,a);
-        if(lv[a]>lv[b]) swap(a,b);
-        for(ll diff=lv[b]-lv[a],i=0; diff; ++i){
-            if(diff&1<<i){
-                diff^=1<<i;
-                goai(ret,b,i);
-            }
-        }
-        if(a==b) return ret;
-        for(ll i=log; i>=0; --i){
-            if(anc[a][i].se!=anc[b][i].se){
-                goai(ret,a,i);
-                goai(ret,b,i);
-            }
-        }
-        goai(ret,a,0);
-        goai(ret,b,0);
-        return ret;
-    }
-};
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ll n,a,b,c;
+    ll a,b,c;
     cin>>n;
-    graph G(n);
     FOR(i,2,n){
         cin>>a>>b>>c;
-        G.adj[a].eb(c,b);
-        G.adj[b].eb(c,a);
+        adj[a].eb(c,b);
+        adj[b].eb(c,a);
     }
-    G.bfs(1);
-    cin>>n;
-    while(n--){
+    bfs(1);
+    ll m;
+    cin>>m;
+    while(m--){
         cin>>a>>b;
-        cout<<G.lca(a,b).fi<<endl;
+        cout<<lca(a,b).fi<<endl;
     }
     return 0;
 }
--- dijkstra_2211.cpp
+++ dijkstra_2211.cpp
@@ -12,50 +12,48 @@ using pll=pair<ll,ll>;
 using vl=vector<ll>;
 using vp=vector<pll>;
 const ll INF=1e18;
+const ll MAXV=1e3;
 
-struct graph{
-    ll n;
-    vector<vp> adj;
-    vl dist,par;
-    graph(ll n):n(n),adj(vector<vp>(n+5)){}
-    void dijk(ll idx){
-        dist=vl(n+5,INF);
-        dist[idx]=0;
-        par=vl(n+5);
-        priority_queue<pll,vp,greater<pll>> pq;
-        pq.emplace(0,idx);
-        ll a,b,c,d;
-        while(!pq.empty()){
-            tie(a,b)=pq.top();
-            pq.pop();
-            if(dist[b]!=a) continue;
-            for(pll& p:adj[b]){
-                tie(c,d)=p;
-                c+=a;
-                if(dist[d]>c){
-                    dist[d]=c;
-                    par[d]=b;
-                    pq.emplace(c,d);
-                }
+ll n,m;
+vp adj[MAXV+5];
+ll dist[MAXV+5];
+ll par[MAXV+5];
+
+void dijk(ll idx){
+    FOR(i,1,n) dist[i]=INF;
+    dist[idx]=0;
+    priority_queue<pll,vp,greater<pll>> pq;
+    pq.emplace(0,idx);
+    while(!pq.empty()){
+        ll a,b;
+        tie(a,b)=pq.top();
+        pq.pop();
+        if(dist[b]!=a) continue;
+        for(pll& p:adj[b]){
+            ll c,d;
+            tie(c,d)=p;
+            c+=a;
+            if(dist[d]>c){
+                dist[d]=c;
+                par[d]=b;
+                pq.emplace(c,d);
             }
         }
     }
-};
+}
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ll n,m;
     cin>>n>>m;
-    graph G(n);
     while(m--){
         ll a,b,c;
         cin>>a>>b>>c;
-        G.adj[a].eb(c,b);
-        G.adj[b].eb(c,a);
+        adj[a].eb(c,b);
+        adj[b].eb(c,a);
     }
-    G.dijk(1);
+    dijk(1);
     cout<<n-1<<endl;
-    FOR(i,2,n) cout<<i<<' '<<G.par[i]<<endl;
+    FOR(i,2,n) cout<<i<<' '<<par[i]<<endl;
     return 0;
 }
