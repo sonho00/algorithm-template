8386928ff957026f1304c1562616bc2023f311e6
sonho00 <sonho00@naver.com>
2020-10-30 10:30:50 +0900

    Add extended CRT

--- chinese_remainder_theorem_12971.cpp
+++ chinese_remainder_theorem_12971.cpp
@@ -1,13 +1,21 @@
 #include<bits/stdc++.h>
+#define FOR(i,a,b) for(ll i=a; i<=b; ++i)
+#define ALL(x) x.begin(),x.end()
+#define pb push_back
+#define eb emplace_back
 #define fi first
 #define se second
+#define endl '\n'
 using namespace std;
 using ll=long long;
 using pll=pair<ll,ll>;
+using vl=vector<ll>;
+using vp=vector<pll>;
+const ll INF=1e18;
 
-vector<ll> ext_euc(ll a,ll b){
-    vector<ll> ret={1,0,a};
-    vector<ll> tmp={0,1,b};
+vl ext_euc(ll a,ll b){
+    vl ret={1,0,a};
+    vl tmp={0,1,b};
     while(tmp[2]){
         ll q=ret[2]/tmp[2];
         ret={ret[0]-q*tmp[0],ret[1]-q*tmp[1],ret[2]-q*tmp[2]};
@@ -23,7 +31,7 @@ write x=a mod n as pll{a,n}
 input = array of {a_i,n_i}
 output = {x,N}
 */
-pll crt(vector<pll> arr){
+pll crt(vp arr){
     ll r=0,n=1;
     for(pll& p:arr){
         p.fi=(p.se+p.fi%p.se)%p.se;
@@ -32,17 +40,11 @@ pll crt(vector<pll> arr){
     for(pll& p:arr) r=(r+n/p.se*ext_euc(n/p.se,p.se)[0]*p.fi%n)%n;
     return {(n+r%n)%n,n};
 }
-/*
-CRT 확장
-n_i가 서로소가 아닐 때도 사용 가능
-복잡도 ~ k*sqrt(max(n_i))
-k는 mod식 개수
-*/
-pll ext_crt(vector<pll> arr){
-    vector<pll> tmp;
-    ll m=1,a=0,b=1,c;
+pll ext_crt(vp arr){
+    vp tmp;
+    ll m=1,a=0,b=1;
     for(pll& p:arr){
-        c=1;
+        ll c=1;
         while(!(p.se&1)){
             p.se>>=1;
             c<<=1;
@@ -57,14 +59,14 @@ pll ext_crt(vector<pll> arr){
         }
         m=max(m,p.se);
     }
-    if(b!=1) tmp.emplace_back(a,b);
+    if(b!=1) tmp.eb(a,b);
     ll sqm=sqrt(m)+3;
     vector<bool> visit(sqm+5);
     for(ll i=3; i<=sqm; i+=2){
         if(visit[i]) continue;
         a=0,b=1;
         for(pll& p:arr){
-            c=1;
+            ll c=1;
             while(!(p.se%i)){
                 p.se/=i;
                 c*=i;
@@ -78,7 +80,7 @@ pll ext_crt(vector<pll> arr){
                 if((p.fi-a)%c) return {-1,-1};
             }
         }
-        if(b!=1) tmp.emplace_back(a,b);
+        if(b!=1) tmp.eb(a,b);
         for(ll j=i*i; j<=sqm; j+=i<<1){
             visit[j]=1;
         }
@@ -92,7 +94,7 @@ pll ext_crt(vector<pll> arr){
                 p.se=1;
             }
         }
-        tmp.emplace_back(a,b);
+        tmp.eb(a,b);
     }
     return crt(tmp);
 }
@@ -101,10 +103,10 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     ll a[3],b[3];
-    for(ll i=0; i<3; ++i) cin>>a[i];
-    for(ll i=0; i<3; ++i) cin>>b[i];
-    vector<pll> arr;
-    for(ll i=0; i<3; ++i) arr.emplace_back(b[i],a[i]);
+    FOR(i,0,2) cin>>a[i];
+    FOR(i,0,2) cin>>b[i];
+    vp arr;
+    FOR(i,0,2) arr.eb(b[i],a[i]);
     cout<<ext_crt(arr).fi;
     return 0;
 }
