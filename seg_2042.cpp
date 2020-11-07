e05c8ab306ba322f414dc50302fbfe48298abd1a
sonho00 <sonho00@naver.com>
2020-11-07 20:33:23 +0900

    Non recursive

--- seg_2042.cpp
+++ seg_2042.cpp
@@ -5,68 +5,56 @@
 #define eb emplace_back
 #define fi first
 #define se second
+#define endl '\n'
 using namespace std;
 using ll=long long;
 using pll=pair<ll,ll>;
+using vl=vector<ll>;
+using vp=vector<pll>;
 const ll INF=1e18;
-const ll MAX=1e6;
-const ll MOD=1e9+7;
-const ll LOG=20;
 
-struct node{
-    ll val;
-    node(ll val=0):val(val){}
-    void updt(node& a,node& b){
-        val=a.val+b.val;
-    }
-};
+template<typename T>
 struct seg{
+    vector<T> v;
     ll base;
-    vector<node> v;
-    seg(ll n,ll* arr=NULL){
+    seg(ll n){
         base=1;
-        while(base<n) base<<=1;
+        while(base<=n) base<<=1;
         v.resize(base<<1);
-        if(arr==NULL) return;
-        FOR(i,1,n) v[base+i-1]=arr[i];
+    }
+    void build(){
         for(ll i=base-1; i; --i){
-            v[i].updt(v[i<<1],v[i<<1|1]);
+            v[i]=v[i<<1]+v[i<<1|1];
         }
     }
-    void updt(ll idx,ll val){
-        idx+=base-1;
-        v[idx].val=val;
-        while(idx){
-            idx>>=1;
-            v[idx].updt(v[idx<<1],v[idx<<1|1]);
+    void updt(ll idx,T val){
+        for(v[idx+=base]=val; idx>=2; idx>>=1){
+            v[idx>>1]=v[idx]+v[idx^1];
         }
     }
-    ll query(ll l,ll r){return query(l,r,1,base,1);}
-    ll query(ll l,ll r,ll s,ll e,ll vi){
-        if(l<=s && e<=r) return v[vi].val;
-        if(r<s || e<l) return 0;
-        ll m=s+e>>1;
-        ll lval=query(l,r,s,m,vi<<1);
-        ll rval=query(l,r,m+1,e,vi<<1|1);
-        return lval+rval;
+    T query(ll l,ll r){
+        T ret=0;
+        for(l+=base,r+=base+1; l<r; l>>=1,r>>=1){
+            if(l&1) ret+=v[l++];
+            if(r&1) ret+=v[--r];
+        }
+        return ret;
     }
 };
-
-ll arr[MAX+5];
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ll n,m,k,q;
+    ll n,m,k;
     cin>>n>>m>>k;
-    q=m+k;
-    FOR(i,1,n) cin>>arr[i];
-    seg tree(n,arr);
-    while(q--){
+    seg<ll> tree(n);
+    FOR(i,1,n) cin>>tree.v[i+tree.base];
+    tree.build();
+    for(ll q=m+k; q--;){
         ll a,b,c;
         cin>>a>>b>>c;
         if(a==1) tree.updt(b,c);
-        else cout<<tree.query(b,c)<<'\n';
+        else cout<<tree.query(b,c)<<endl;
     }
     return 0;
 }
