88dac597c371e9d4ebe88dd4a0d34cd7517f26d6
sonho00 <sonho00@naver.com>
2020-11-07 20:34:44 +0900

    Typename T

--- coordinate_compression_18870.cpp
+++ coordinate_compression_18870.cpp
@@ -1,37 +1,40 @@
 #include<bits/stdc++.h>
-#define FOR(i,a,b) for(int i=a; i<=b; ++i)
+#define FOR(i,a,b) for(ll i=a; i<=b; ++i)
 #define ALL(x) x.begin(),x.end()
 #define pb push_back
 #define eb emplace_back
 #define fi first
 #define se second
+#define endl '\n'
 using namespace std;
 using ll=long long;
 using pll=pair<ll,ll>;
+using vl=vector<ll>;
+using vp=vector<pll>;
 const ll INF=1e18;
-const ll MAX=1e6;
 
+template<typename T>
 struct cor{
-    vector<ll> v;
-    void make(){
+    vector<T> v;
+    void build(){
         sort(ALL(v));
         v.erase(unique(ALL(v)),v.end());
     }
-    ll operator[](ll val){return lower_bound(ALL(v),val)-v.begin();}
+    ll operator[](T val){return lower_bound(ALL(v),val)-v.begin();}
 };
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     ll n;
-    ll x[MAX+5];
-    cor xc;
+    cor<ll> xc;
     cin>>n;
+    vl x(n+5);
     FOR(i,1,n){
         cin>>x[i];
         xc.v.pb(x[i]);
     }
-    xc.make();
+    xc.build();
     FOR(i,1,n) cout<<xc[x[i]]<<' ';
     return 0;
 }
