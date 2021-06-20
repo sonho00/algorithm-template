b79d9ad90065c87d9778b7882a0b6efff1f22d1f
sonho00 <sonho00@naver.com>
2021-06-20 23:46:09 +0900

    scc

--- scc_2150.cpp
+++ scc_2150.cpp
@@ -1,56 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int v,e;
-vector<int> adj[10101];
-int dfsn;
-int dfn[10101];
-stack<int> s;
-int scn[10101];
-int sccn;
-
-int dfs(int idx){
-    int ret=dfn[idx]=++dfsn;
-    s.push(idx);
-    for(int nxt:adj[idx]){
-        if(!dfn[nxt]) ret=min(ret,dfs(nxt));
-        if(!scn[nxt]) ret=min(ret,dfn[nxt]);
+struct SCC{
+    int n,sccn,dfsn;
+    vector<int> scc,dfn,low;
+    vector<vector<int>> adj;
+    stack<int> s;
+    SCC(int n):n(n),adj(n+5),scc(n+5),dfn(n+5),low(n+5){}
+    void dfs(int idx){
+        low[idx]=dfn[idx]=++dfsn;
+        s.push(idx);
+        for(int nxt:adj[idx]){
+            if(!dfn[nxt]) dfs(nxt);
+            if(!scc[nxt]) low[idx]=min(low[idx],low[nxt]);
+        }
+        if(low[idx]==dfn[idx]){
+            ++sccn;
+            while(1){
+                int tmp=s.top();
+                s.pop();
+                scc[tmp]=sccn;
+                if(tmp==idx) break;
+            }
+        }
     }
-    if(ret==dfn[idx]){
-        ++sccn;
-        int tmp;
-        do{
-            tmp=s.top();
-            s.pop();
-            scn[tmp]=sccn;
-        } while(tmp!=idx);
+    void solve(){
+        for(int i=1; i<=n; ++i){
+            if(!dfn[i]) dfs(i);
+        }
     }
-    return ret;
-}
+};
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
+    int v,e;
     cin>>v>>e;
-    while(e--){
+    SCC scc(v);
+    for(int i=0; i<e; ++i){
         int a,b;
         cin>>a>>b;
-        adj[a].push_back(b);
+        scc.adj[a].push_back(b);
     }
-
+    scc.solve();
+    vector<vector<int>> ans(scc.sccn);
     for(int i=1; i<=v; ++i){
-        if(!dfn[i]) dfs(i);
+        ans[scc.scc[i]-1].push_back(i);
     }
-
-    vector<vector<int>> ans(sccn);
-    for(int i=1; i<=v; ++i){
-        ans[scn[i]-1].push_back(i);
+    for(int i=0; i<scc.sccn; ++i){
+        sort(ans[i].begin(),ans[i].end());
     }
     sort(ans.begin(),ans.end());
-    cout<<ans.size()<<'\n';
-    for(auto& v:ans){
-        for(int x:v) cout<<x<<' ';
+    cout<<scc.sccn<<'\n';
+    for(int i=0; i<scc.sccn; ++i){
+        for(int j:ans[i]){
+            cout<<j<<' ';
+        }
         cout<<"-1\n";
     }
     return 0;
