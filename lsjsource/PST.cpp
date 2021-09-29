368cd6ac7d040266c72c78b81578bf709b41eae8
leesj <31756944+leesj10147@users.noreply.github.com>
2021-09-30 02:05:31 +0900

    Update PST.cpp
    http://boj.kr/5b1d8d8b7e764f78bc604a9291fcaacc

--- lsjsource/PST.cpp
+++ lsjsource/PST.cpp
@@ -1,11 +1,11 @@
 template<typename T> struct PST
 {
     #define L 0
-    #define R 2'000'000'200
-
+    #define R 1'000'000
+    //pst의 범위
     struct vertex{int l, r; T val;};
     vector<vertex> pst;
-    int make_tree(int nx)
+    int make_tree(int nx) // head == nx인 pst를 복사하여 new head를 반환
     {
         if (nx == -1)
         {
@@ -18,11 +18,23 @@ template<typename T> struct PST
             return (int)(pst.size()) - 1;
         }
     }
-    void update(int nx, int idx, T df, int ns = L, int ne = R)
+    void update(int nx, T* arr, int ns = L, int ne = R) // head==nx인 트리의 ns부터 ne까지 한번에 갱신 O(ne-ns)
     {
         if (ns == ne)
         {
-            pst[nx].val = pst[nx].val + df;//
+            pst[nx].val = arr[ns];
+            return;
+        }
+        int mid = (ns + ne) >> 1;
+        update(pst[nx].l = make_tree(pst[nx].l), arr, ns, mid);
+        update(pst[nx].r = make_tree(pst[nx].r), arr, mid + 1, ne);
+        pst[nx].val = pst[pst[nx].l].val + pst[pst[nx].r].val;
+    }
+    void update(int nx, int idx, T df, int ns = L, int ne = R) // head == nx인 트리의 idx를 df로 갱신
+    {
+        if (ns == ne)
+        {
+            pst[nx].val = df;//
             return;
         }
         int mid = (ns + ne) >> 1;
@@ -32,7 +44,7 @@ template<typename T> struct PST
         if (pst[nx].l != -1) pst[nx].val = pst[nx].val + pst[pst[nx].l].val;//
         if (pst[nx].r != -1) pst[nx].val = pst[nx].val + pst[pst[nx].r].val;//
     }
-    T get_sum(int nx, int qs, int qe, int ns = L, int ne = R)
+    T get_sum(int nx, int qs, int qe, int ns = L, int ne = R) // 응
     {
         if (nx == -1) return T();//
         if (ne < qs || qe < ns) return T(); //
