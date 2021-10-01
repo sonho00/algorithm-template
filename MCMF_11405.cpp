beadc05cf2885e33951e48e5f3f8c116878c8a61
sonho00 <sonho00@naver.com>
2021-10-01 11:35:09 +0900

    edit mcmf

--- MCMF_11405.cpp
+++ MCMF_11405.cpp
@@ -1,90 +1,83 @@
 #include<bits/stdc++.h>
 #define FOR(i,a,b) for(ll i=a; i<=b; ++i)
-#define ALL(x) x.begin(),x.end()
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
-#define endl '\n'
 using namespace std;
 using ll=long long;
 using pll=pair<ll,ll>;
-using vl=vector<ll>;
-using vp=vector<pll>;
 const ll INF=1e18;
 
 struct MCMF{
+    struct Edge{
+        ll par,nxt,cap,cost,flow;
+        Edge(ll par,ll nxt,ll cap,ll cost):par(par),nxt(nxt),cap(cap),cost(cost),flow(0){}
+    };
     ll n,src,sink;
-    vector<vl> cap,flow;
-    vector<vp> adj;
-    MCMF(ll n):n(n),src(n+1),sink(n+2),
-    cap(vector<vl>(n+5,vl(n+5))),
-    flow(vector<vl>(n+5,vl(n+5))),
-    adj(vector<vp>(n+5))
-    {}
-    MCMF(ll n,ll src,ll sink):n(n),src(src),sink(sink),
-    cap(vector<vl>(n+5,vl(n+5))),
-    flow(vector<vl>(n+5,vl(n+5))),
-    adj(vector<vp>(n+5))
-    {}
+    vector<ll> st,lv;
+    vector<Edge> edge;
+    MCMF(ll n):n(n),src(n+1),sink(n+2),st(n+5,-1){}
+    void addEdge(ll a,ll b,ll cap,ll cost){
+        edge.emplace_back(st[a],b,cap,cost);
+        st[a]=edge.size()-1;
+        edge.emplace_back(st[b],a,0,-cost);
+        st[b]=edge.size()-1;
+    }
     pll spfa(){
-        vl dist(n+5,INF);
+        vector<ll> dist(n+5,INF);
         dist[src]=0;
-        vl par(n+5);
         vector<bool> inq(n+5);
         inq[src]=1;
         queue<ll> q;
         q.push(src);
+        vector<ll> par;
         while(!q.empty()){
-            ll top=q.front();
+            ll idx=q.front();
             q.pop();
-            inq[top]=0;
-            for(pll& p:adj[top]){
-                ll a,b;
-                tie(a,b)=p;
-                if(flow[top][b]<cap[top][b]){
-                    ll d=dist[top]+a;
-                    if(dist[b]>d){
-                        dist[b]=d;
-                        par[b]=top;
-                        if(!inq[b]){
-                            inq[b]=1;
-                            q.push(b);
+            inq[idx]=0;
+            for(ll i=st[idx]; i!=-1; i=edge[i].par){
+                Edge& e=edge[i];
+                if(e.flow<e.cap){
+                    if(dist[e.nxt]>dist[idx]+e.cost){
+                        dist[e.nxt]=dist[idx]+e.cost;
+                        par[e.nxt]=i;
+                        if(!inq[e.nxt]){
+                            inq[e.nxt]=1;
+                            q.push(e.nxt);
                         }
                     }
                 }
             }
         }
-        if(!par[sink]) return pll(0,0);
-        ll f=INF,v=sink,p;
-        while(v!=src){
-            p=par[v];
-            f=min(f,cap[p][v]-flow[p][v]);
-            v=p;
+        if(!par[sink]) return {0,0};
+        ll flow=INF;
+        for(ll idx=sink; idx!=src; idx=edge[par[idx]^1].nxt){
+            Edge& e=edge[par[idx]];
+            flow=min(flow,e.cap-e.flow);
         }
-        v=sink;
-        while(v!=src){
-            p=par[v];
-            flow[p][v]+=f;
-            flow[v][p]-=f;
-            v=p;
+        for(ll idx=sink; idx!=src; idx=edge[par[idx]^1].nxt){
+            Edge& e=edge[par[idx]];
+            e.flow+=flow;
+            edge[par[idx]^1].flow-=flow;
         }
-        return pll(f*dist[sink],f);
+        return pll(flow*dist[sink],flow);
     }
-    pll operator()(){
+    pll solve(){
         pll ret;
         while(1){
-            pll tmp=spfa();
-            if(!tmp.se) break;
-            ret.fi+=tmp.fi;
-            ret.se+=tmp.se;
+            auto[a,b]=spfa();
+            if(!b) break;
+            ret.first+=a;
+            ret.second+=b;
         }
         return ret;
     }
-    void addEdge(ll a,ll b,ll capa,ll cst){
-        cap[a][b]=capa;
-        adj[a].eb(cst,b);
-        adj[b].eb(-cst,a);
+    void print(){
+        for(ll i=1; i<=n+2; ++i){
+            cout<<i<<'\n';
+            for(ll j=st[i]; j!=-1; j=edge[j].par){
+                Edge& e=edge[j];
+                cout<<j<<' '<<e.par<<' '<<e.nxt<<' '<<e.cap<<' '<<e.cost<<' '<<e.flow<<'\n';
+            }
+            cout<<'\n';
+        }
     }
 };
 
@@ -109,6 +102,8 @@ int main(){
         cin>>k;
         mcmf.addEdge(i,j,INF,k);
     }
-    cout<<mcmf().fi;
+    mcmf.print();
+    return 0;
+    cout<<mcmf.solve().first;
     return 0;
 }
